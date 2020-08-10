588a5409554a5e106c7aef1a4ac20bdcc90c63da
L3MON4D3 <Simljk@outlook.de>
2020-08-10 14:44:54 +0200

    Add Texture and model to LightningConductor.

--- src/main/java/com/L3MON4D3/lightningmod/block/LightningConductorBlock.java
+++ src/main/java/com/L3MON4D3/lightningmod/block/LightningConductorBlock.java
@@ -1,5 +1,7 @@
 package com.L3MON4D3.lightningmod.block;
 
+import java.util.stream.Stream;
+
 import com.L3MON4D3.lightningmod.LightningMod;
 import com.L3MON4D3.lightningmod.init.ModTileEntities;
 import com.L3MON4D3.lightningmod.tileentity.LightningConductorTileEntity;
@@ -9,7 +11,6 @@ import org.apache.logging.log4j.Logger;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.tileentity.TileEntity;
@@ -17,16 +18,26 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
-public class LightningConductorBlock extends HorizontalBlock {
+public class LightningConductorBlock extends Block {
     public static final Logger LOGGER = LogManager.getLogger(LightningMod.MODID);
+    
+    private static final VoxelShape SHAPE = Stream.of(
+        Block.makeCuboidShape(7, 5, 7, 9, 10, 9),
+        Block.makeCuboidShape(3, 3, 3, 13, 5, 13),
+        Block.makeCuboidShape(0, 0, 0, 16, 3, 16)
+    ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
 
-   public LightningConductorBlock(Block.Properties builder) {
-      super(builder);
-   }
+    public LightningConductorBlock(Block.Properties builder) {
+        super(builder);
+    }
 
     @Override
     public ActionResultType onBlockActivated(final BlockState state, final World worldIn,
@@ -39,6 +50,13 @@ public class LightningConductorBlock extends HorizontalBlock {
 		}
 		return ActionResultType.SUCCESS;
     }
+
+	@Override
+	public VoxelShape getShape(final BlockState state, final IBlockReader worldIn,
+    final BlockPos pos, final ISelectionContext context) {
+		return SHAPE;
+	}
+
 
     @Override
     public boolean hasTileEntity(final BlockState state) {
